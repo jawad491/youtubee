c2e8ccd9da8ce4e73ac764e370278f1396b2550c
jawad491 <quizzteam75@gmail.com>
2022-12-14 01:05:37 +0500

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class First extends StatelessWidget{
      child: Scaffold(
        backgroundColor: Colors.white,
        appBar: AppBar(backgroundColor: Colors.lightGreenAccent.shade700, centerTitle:true,
-         title: Text('App Name & Icon', style: TextStyle(fontSize: 29,fontWeight: FontWeight.bold),),),
+         title: Text('Chang in github', style: TextStyle(fontSize: 29,fontWeight: FontWeight.bold),),),
        body: ListView(
          children: [
            Card(
@@ -38,13 +38,12 @@ class First extends StatelessWidget{
 
 
 
-// git --version                                             To Confirm that Git is installed in your PC
+// git --version                              To Confirm that Git is installed in your PC
 // git config --global user.name "jawad491"                  To initialize
 // git config --global user.email "quizzteam75@gmail.com"    To initialize
 // git init                                                  To create Empty repository locally
 // git add .                                                 To add current project in that repository
 //git commit -m "My Commit ha"                               To commit changes
-
 
 
 
